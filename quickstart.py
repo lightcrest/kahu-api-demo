384d3f97ead7b62b4e58ca344024f7f8c826b3a3
Alexander Guy <alexander.guy@lightcrest.com>
2015-05-07 16:03:12 -0700

    Use instance URLs, and properly urljoin them.

--- quickstart.py
+++ quickstart.py
@@ -3,6 +3,7 @@
 import sys
 import logging
 import time
+from urlparse import urljoin
 
 import util
 
@@ -135,25 +136,24 @@ def create_vm(name, size, profile):
     r = session.post(tenant_url + "/compute/instance/", files=form)
     util.check_response(r, expected_statuses=[201])
 
-    instance_uri = r.headers['location']
-
+    instance_url = urljoin(api_url, r.headers['location'])
     info = r.json()
-    logging.info("New instance located at '%s'." % (instance_uri))
+    logging.info("New instance located at '%s'." % (instance_url))
     logging.debug(info)
 
     logging.info("Tagging instance with new name.")
-    r = session.post(api_url + instance_uri + "/tag/", files=dict(name="name",
-                                                                       value=name))
+    r = session.post(instance_url + "/tag/", files=dict(name="name",
+                                                        value=name))
     util.check_response(r, expected_statuses=[201])
 
-    return instance_uri
+    return instance_url
 
 
-instance_uris = []
+instance_urls = []
 
-instance_uris.append(create_vm("New Developer Instance", size_ids[0], profile_ids[0]))
-instance_uris.append(create_vm("Another Developer Instance", size_ids[0], profile_ids[0]))
-instance_uris.append(create_vm("Yet Another Developer Instance", size_ids[0], profile_ids[0]))
+instance_urls.append(create_vm("New Developer Instance", size_ids[0], profile_ids[0]))
+instance_urls.append(create_vm("Another Developer Instance", size_ids[0], profile_ids[0]))
+instance_urls.append(create_vm("Yet Another Developer Instance", size_ids[0], profile_ids[0]))
 
 instance_report()
 
@@ -166,14 +166,14 @@ step("Start all of our new Virtual Machines")
 #     =====================================
 #
 
-def control_vm(uri, action):
-    logging.info("Running action '%s' at '%s'" % (action, uri))
-    r = session.post(api_url + uri + "/" + action)
+def control_vm(url, action):
+    logging.info("Running action '%s' at '%s'" % (action, url))
+    r = session.post(url + "/" + action)
     # If the status code is 400, it means that the instance was already in that state.
     util.check_response(r, expected_statuses=[200, 400])
 
-for uri in instance_uris:
-    control_vm(uri, "start")
+for url in instance_urls:
+    control_vm(url, "start")
 
 util.end_step()
 
@@ -197,7 +197,7 @@ step("Waiting (up to 180 seconds) For IPv4 Addresses To Be Assigned...")
 logging.info("Waiting for all of our instances to be assigned IPv4 addresses...")
 
 start = time.time()
-addresses = [None] * len(instance_uris)
+addresses = [None] * len(instance_urls)
 
 found = 0
 while True:
@@ -205,13 +205,13 @@ while True:
         logging.error("Timing out waiting for IPv4 addresses.")
         break
 
-    for idx, uri in enumerate(instance_uris):
+    for idx, url in enumerate(instance_urls):
         if addresses[idx] != None:
             continue
 
-        logging.info("Attempting to get the address for instance '%s'" % (uri,))
-        uri = api_url + uri + "/network/" + public_interface + "/address"
-        r = session.get(uri)
+        logging.info("Attempting to get the address for instance '%s'" % (url,))
+        url = url + "/network/" + public_interface + "/address"
+        r = session.get(url)
         if r.status_code != 200:
             continue
 
@@ -219,7 +219,7 @@ while True:
 
         found += 1
 
-    if found >= len(instance_uris):
+    if found >= len(instance_urls):
         break
 
     logging.debug("Sleeping for 5 seconds...")
@@ -241,15 +241,15 @@ step("Deleting all of the Virtual Machines We Created")
 #
 
 def delete_vm(uri):
-    control_vm(uri, "stop")
+    control_vm(url, "stop")
 
     logging.info("Deleting instance at '%s'" % (uri))
-    r = session.delete(api_url + uri)
+    r = session.delete(url)
     util.check_response(r)
 
 
-for uri in instance_uris:
-    delete_vm(uri)
+for url in instance_urls:
+    delete_vm(url)
 
 util.end_step()
 
